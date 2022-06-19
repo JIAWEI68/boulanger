8c0e119014a0711d43364eb43ecd9b8787551390
JIAWEI68 <ang.jiawei56@gmail.com>
2022-06-19 18:46:30 +0800

    login function done

--- lib/screens/login_sign_up_screen.dart
+++ lib/screens/login_sign_up_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/screens/login_screen.dart';
 
 class LoginAndSignUpScreen extends StatelessWidget {
   const LoginAndSignUpScreen({Key? key}) : super(key: key);
@@ -6,12 +7,14 @@ class LoginAndSignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Container(
         child: Column(
           children: [Icon(Icons.account_circle), ElevatedButton(onPressed: () {}, child: const Text("Sign Up"), ), ElevatedButton(child: Text("Login"), onPressed: (){},)],
         ),
       ),
     );
+  }
+  void goToLogin(BuildContext context){
+    Navigator.push(context, MaterialPageRoute(builder: (context) =>   const LoginScreen()));
   }
 }
